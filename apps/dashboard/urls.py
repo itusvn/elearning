e5e7efebe1636da3956d9e7379b0922817baf0de
Thu Nguyen <thu.nguyen@pninformatics.com>
2022-09-13 18:03:26 +0700

    update layout

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 
-from apps.basebackend import views_table
 from . import views
 
 app_name = 'dashboard'
 
 urlpatterns = [
-    path('', views.index, name='dashboard'),
-    path('tables', views_table.TableView.as_view(), name='table'),
+    path('', views.index, name='dashboard')
 ]
--- apps/quiz/urls.py
+++ apps/quiz/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from apps.basebackend import views_table
 
 app_name = 'quiz'
 
-urlpatterns = [   
+urlpatterns = [
+    path('category', views_table.TableView.as_view(), name='quiz-category'),
 ]
