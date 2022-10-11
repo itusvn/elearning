a0f5d69dcb920c4eb8958d36e5ba605930863027
Thu Nguyen <thu.nguyen@pninformatics.com>
2022-10-11 11:46:46 +0700

    update front end

--- apps/quiz/views.py
+++ apps/quiz/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from apps.basebackend.views_table import TableView
 # Create your views here.
 
 class Category(TableView):
     template_name = 'quiz/category/index.html'
+
+
+def home(request):
+    if request.user.is_authenticated:
+        if request.user.is_teacher:
+            return redirect('dashboard')
+        elif request.user.is_student:
+            return redirect('students:quiz_list')
+        else:
+            return redirect('admin:index')
+    return render(request,'front/index.html')
--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from apps.quiz import views as views_quiz
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('quiz/', include('apps.quiz.urls')),
     path('dashboard/', include('apps.dashboard.urls')),
     path('authenticate/', include('apps.authenticate.urls')),
+    path('', views_quiz.home),
 ]
